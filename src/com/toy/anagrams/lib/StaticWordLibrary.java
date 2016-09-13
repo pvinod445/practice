1c4df5b0b917a01f1532c141eea0062ea7938860
S525754 <S525754@NW36319.nwmissouri.edu>
2016-09-12 21:25:48 -0500

    added rathnakar name in StaticWordLibrary.java

--- src/com/toy/anagrams/lib/StaticWordLibrary.java
+++ src/com/toy/anagrams/lib/StaticWordLibrary.java
@@ -37,6 +37,7 @@ package com.toy.anagrams.lib;
 final class StaticWordLibrary extends WordLibrary {
 
     private static final String[] WORD_LIST = {
+        "Rathnakar",
         "Reddy",
         "abstraction",
         "ambiguous",
@@ -85,6 +86,7 @@ final class StaticWordLibrary extends WordLibrary {
         "traditional"};
 
     private static final String[] SCRAMBLED_WORD_LIST = {
+        "Ettedi",
         "eddyR",
         "batsartcoin",
         "maibuguos",
